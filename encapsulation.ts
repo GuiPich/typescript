fff4ce318b9789c3e0170f7a75503318ef88237a
GuiPich <110974962+GuiPich@users.noreply.github.com>
2023-03-20 08:20:37 +0100

    new challenge

---
+++ encapsulation.ts
@@ -0,0 +1,32 @@
+import assert from 'assert';
+
+// ... Ton code ici ...
+
+class BankCustomer {
+    private name: string;
+    private pin: number;
+
+    constuctor(name: string, pin: number) {
+        this.name = name;
+        this.pin = pin;
+    }
+
+    getname(): void {
+        console.log(`${this.name}`);
+    }
+    verifyPinInput(a): boolean {
+        if (this.pin === a) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// Tests
+
+const customer = new BankCustomer('John Doe', '3579');
+assert.equal(typeof customer.getName, 'function');
+assert.equal(typeof customer.verifyPinInput, 'function');
+assert.equal(customer.getName(), 'John Doe');
+assert.ok(customer.verifyPinInput('3579'));
